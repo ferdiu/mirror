38f8cfeaf1b1da3fa37f47b4b157e031e01c04bc
ferdiu <ferdiu.manzella@gmail.com>
2025-02-23 15:25:09 +0100

    redirect to HTTPS instead of duplicate application
    Signed-off-by: ferdiu <ferdiu.manzella@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,12 @@ import https from "https";
 import { Server } from "socket.io";
 import { registerSocketEvents } from "./controllers/socket.controller";
 
+// Use the port 3000 by default, or the one specified in the PORT environment variable
+const portHttp = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT) : 3000;
+const portHttps = process.env.HTTPS_PORT
+  ? parseInt(process.env.HTTPS_PORT)
+  : 3443;
+
 // Load SSL/TLS certificates
 const privateKey = fs.readFileSync("key.pem", "utf8");
 const certificate = fs.readFileSync("cert.pem", "utf8");
@@ -16,21 +22,29 @@ const app = express();
 // Serve static files from the "public" directory
 app.use(express.static("public"));
 
+// Redirect HTTP requests to HTTPS
+const redirectApp = express();
+redirectApp.use((req, res, next) => {
+  if (!req.secure) {
+    const h = req.headers.host?.endsWith(":" + portHttp.toString())
+      ? req.headers.host?.endsWith(":" + portHttp.toString())
+      : req.headers.host;
+    return res.redirect(
+      `https://${h}:${portHttps == 443 ? "" : portHttps}${req.url}`
+    );
+  }
+  next();
+});
+
 // Create an HTTP and an HTTPs server
-const serverHttp = http.createServer(app);
+const serverHttp = http.createServer(redirectApp);
 const serverHttps = https.createServer(credentials, app);
 
 // Attach a Socket.IO server
-const ioHttp = new Server(serverHttp);
 const ioHttps = new Server(serverHttps);
 
 // Register all socket events
-registerSocketEvents(ioHttp);
 registerSocketEvents(ioHttps);
-
-// Use the port 3000 by default, or the one specified in the PORT environment variable
-const portHttp = process.env.HTTP_PORT ?? 3000;
-const portHttps = process.env.HTTPS_PORT ?? 3443;
 
 // Start the HTTP server
 serverHttp.listen(portHttp, () => {
